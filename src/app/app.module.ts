619d16af691efbfeccc7e7f0449dacacc122f1e4
BawanthaLaptop <bawanthaperera1999@gmail.com>
2021-07-14 10:25:39 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/database/user.entity';
 import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
