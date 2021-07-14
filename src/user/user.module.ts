d0968a06283da3dd9615900b081fc2c93696d8e1
BawanthaLaptop <bawanthaperera1999@gmail.com>
2021-07-14 16:42:40 +0530

    Update user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/database/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity])
+  ],
   controllers: [UserController],
   providers: [UserService]
 })
